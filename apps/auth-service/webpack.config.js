36d8c0b47c12d344afb8335547cde68080a2e0c0
bereket-7 <bekhonelign@gmail.com>
2026-01-30 13:38:38 +0300

    package refactor for nx apps structure

---
+++ apps/auth-service/webpack.config.js
@@ -0,0 +1,29 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  output: {
+    path: join(__dirname, 'dist'),
+    clean: true,
+    ...(process.env.NODE_ENV !== 'production' && {
+      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    }),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: false,
+      sourceMap: true,
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^pg-native$/,
+    }),
+  ],
+};
