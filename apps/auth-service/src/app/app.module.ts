260dd94e16f7b8850daa3ccee3c585e56a97bd7a
bereket-7 <bekhonelign@gmail.com>
2026-02-05 11:20:21 +0300

    feat: initialize authentication service with core module, JWT strategy, and request throttling.

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from './config/config.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [ConfigModule, AuthModule],
+  imports: [
+    ConfigModule,
+    AuthModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
