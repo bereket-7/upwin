f9d0bcb76387873d8471d402bdcd52eda2c447a5
bereket-7 <bekhonelign@gmail.com>
2026-02-11 11:47:58 +0300

    shared auth libs

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,5 +1,7 @@
 export * from './lib/types';
 export * from './lib/utils';
+export * from './lib/auth';
+export * from './lib/database';
 
 // Legacy export for backward compatibility
 export * from './lib/shared.js';
