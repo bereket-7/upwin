ebc00df53cd3d5fc4b83c898348fec5e48dfbca8
bereket-7 <bekhonelign@gmail.com>
2026-02-11 12:06:02 +0300

    auth controller module, service guard shared integration

--- apps/auth-service/src/app/auth/guards/jwt-auth.guard.ts
+++ apps/auth-service/src/app/auth/guards/jwt-auth.guard.ts
@@ -1,25 +1,14 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtAuthGuard as SharedJwtAuthGuard } from '@org/shared';
 import { TokenBlacklistService } from '../token-blacklist.service';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private tokenBlacklistService: TokenBlacklistService) {
-    super();
-  }
-
-  override handleRequest(err: any, user: any, info: any, context: any) {
-    const request = context.switchToHttp().getRequest();
-    const token = request.headers.authorization?.replace('Bearer ', '');
-    
-    if (token && this.tokenBlacklistService.isTokenBlacklisted(token)) {
-      throw new UnauthorizedException('Token has been revoked');
-    }
-    
-    if (err || !user) {
-      throw err || new UnauthorizedException();
-    }
-    
-    return user;
+export class JwtAuthGuard extends SharedJwtAuthGuard {
+  constructor(
+    tokenBlacklistService: TokenBlacklistService,
+    reflector: Reflector
+  ) {
+    super(tokenBlacklistService, reflector);
   }
 }
