1b6884946c262b42577cb2c241c79ae1b116a60e
bereket-7 <bekhonelign@gmail.com>
2026-03-02 11:02:45 +0300

    fix prisma webpack

--- apps/profile-service/webpack.config.js
+++ apps/profile-service/webpack.config.js
@@ -3,46 +3,45 @@ const { join } = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  target: 'node',
+
   output: {
     path: join(__dirname, 'dist'),
     clean: true,
-    ...(process.env.NODE_ENV !== 'production' && {
-      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    }),
   },
 
   resolve: {
-    alias: {
-      '@org/shared': join(__dirname, '../../libs/shared/src/index.ts'),
-    },
+    extensions: ['.ts', '.js'],
+  },
+
+  externals: {
+    '@prisma/client': 'commonjs @prisma/client',
+    'pg': 'commonjs pg',
+  },
+
+  optimization: {
+    minimize: false,
   },
 
   plugins: [
     new NxAppWebpackPlugin({
-    target: 'node',
-    compiler: 'tsc',
-    main: './src/main.ts',
-    tsConfig: './tsconfig.app.json',
-    assets: ['./src/assets'],
-    optimization: false,
-    outputHashing: 'none',
-    generatePackageJson: true,
-    sourceMap: true,
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
 
-    externalDependencies: 'all',
-  }),
+      assets: ['./src/assets'],
+
+      optimization: false,
+      outputHashing: 'none',
+
+      generatePackageJson: true,
+
+      sourceMap: true,
+    }),
 
     new webpack.IgnorePlugin({
-      resourceRegExp: /^pg-native$/,
-    }),
-    new webpack.IgnorePlugin({
-      resourceRegExp: /^@nestjs\/microservices$/,
-    }),
-    new webpack.IgnorePlugin({
-      resourceRegExp: /^@nestjs\/websockets$/,
-    }),
-    new webpack.IgnorePlugin({
-      resourceRegExp: /^@nestjs\/platform-socket.io$/,
+      resourceRegExp: /^@nestjs\/(microservices|websockets|platform-socket.io)$/,
     }),
   ],
 };
